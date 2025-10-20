00cdac6b02a3ed645c653b1f95462364fb9eee97
Hyodong90 <ct99610138@gmail.com>
2025-10-20 09:54:22 +0900

    키보드추가

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -13,14 +13,62 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[400],
       appBar: AppBar(
         title: Text(
           "효동's tasks",
           style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
         ),
       ),
-      body: Column(
-        children: [Text("아직 할 일이 없음"), Text("할 일을 추가하고 $Title 에서 할 일을 추적하세요.")],
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Container(
+          height: 230,
+          width: double.infinity,
+
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Column(
+            children: [
+              SizedBox(height: 12),
+              Image.asset('assets/1.webp', width: 100, height: 100),
+              SizedBox(height: 12),
+              Text(
+                "아직 할 일이 없음",
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 12),
+              Text(
+                "할 일을 추가하고 Title 에서 \n 할 일을 추적하세요.",
+                style: TextStyle(fontSize: 14, height: 1.5),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            builder: (context) {
+              return Padding(
+                padding: const EdgeInsets.only(top: 12, left: 20, right: 20),
+                
+                child: 
+                TextField(
+                  decoration: InputDecoration(labelText: "새 할 일"),
+                  
+                ),
+              );
+            },
+          );
+        },
+        backgroundColor: Colors.red,
+        shape: CircleBorder(),
+        child: Icon(Icons.add, color: Colors.white, size: 24),
       ),
     );
   }
